4b8bc0c8f004ef53e3489eef47f9e3b1499b5ad6
jonnier <jonnierosjitas@gmail.com>
2024-08-24 16:06:27 -0500

    build thing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 import clerk from '@clerk/astro';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [clerk()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
